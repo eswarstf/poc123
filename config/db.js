546a2ca0680be6d1cb23bab87078a39a8f8a1159
eswarstf <VINAYSINGH DODDAPANENI>
2019-11-20 23:21:08 +0530

    minor changes

--- config/db.js
+++ config/db.js
@@ -4,14 +4,14 @@ var mysql = require('mysql');
 
 //local mysql db connection
 var connection = mysql.createConnection({
-    host: '166.62.28.83',
+     host: '166.62.28.83',
     user: 'kaburulu_admin',
     password: 'AkixdV@EF8291',
-    database: 'phutBoxDB'
+    database: 'phutBoxDB' 
     // host: 'localhost',
     // user: 'root',
     // password: '',
-    // database: 'test2'
+    // database: 'test1'
 });
 connection.connect(function (err) {
     if (err) {throw err;}else{
